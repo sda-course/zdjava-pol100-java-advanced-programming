7e02031bc9ea23dcd4a90262bbc580802cbbaf06
cs <siwon.cezary@gmail.com>
2021-07-26 22:13:54 +0200

    init

--- src/main/java/repository/CityRepository.java
+++ src/main/java/repository/CityRepository.java
@@ -6,20 +6,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.ZipInputStream;
 
-public class CityRepository implements AutoCloseable{
+public class CityRepository implements AutoCloseable {
     private final List<City> cities;
 
     private CityRepository(List<City> cities) {
         this.cities = cities;
     }
 
-    public static CityRepository from(InputStream input){
+    public static CityRepository from(InputStream input) throws IOException {
         return loadCities(input);
     }
 
@@ -27,25 +27,33 @@ public class CityRepository implements AutoCloseable{
         return loadCitiesFromZip(input);
     }
 
-    public Stream<City> findAll(){
+    public Stream<City> findAll() {
         return cities.stream();
     }
 
-    private static CityRepository loadCities(InputStream input){
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input, Charset.forName("UTF-8")));
-        return new CityRepository(reader.lines().map(line -> City.parse(line, "\\t")).collect(Collectors.toList()));
+    private static CityRepository loadCities(InputStream input) throws IOException {
+        try (
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))
+        ) {
+            return new CityRepository(reader.lines().map(line -> City.parse(line, "\\t")).collect(Collectors.toList()));
+        } catch (IOException e) {
+            throw e;
+        }
     }
 
     private static CityRepository loadCitiesFromZip(InputStream input) throws IOException {
-        if (input == null){
+        if (input == null) {
             throw new IllegalArgumentException("Strumień nie może być null! Prawdopodobnie podano ścieżkę do nieistniejącego pliku!");
         }
-        ZipInputStream zipInputStream = new ZipInputStream(input);
-        if (zipInputStream.getNextEntry() != null) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(zipInputStream));
-            return new CityRepository(reader.lines().map(line -> City.parse(line, "\\t")).collect(Collectors.toList()));
-        } else {
-            throw new IOException("Nie można odczytać archiwum zip, brak pliku. W archiwum powinien znajdować się jeden plik!");
+        try (
+                ZipInputStream zipInputStream = new ZipInputStream(input);
+        ) {
+            if (zipInputStream.getNextEntry() != null) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(zipInputStream));
+                return new CityRepository(reader.lines().map(line -> City.parse(line, "\\t")).collect(Collectors.toList()));
+            } else {
+                throw new IOException("Nie można odczytać archiwum zip, brak pliku. W archiwum powinien znajdować się jeden plik!");
+            }
         }
     }
 
