478a8890ec42dbb9c65f9ccc892f262cf5d0d29c
cs <siwon.cezary@gmail.com>
2021-07-27 09:37:05 +0200

    init

--- src/main/java/app/PersonApp.java
+++ src/main/java/app/PersonApp.java
@@ -1,9 +1,20 @@
 package app;
 
+import model.Person;
 import repository.PersonRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PersonApp {
     public static void main(String[] args) {
         PersonRepository repository = new PersonRepository();
+        List<Person> people = repository.findAll()
+                .filter(person -> person.getHeight() > 170)
+                .collect(Collectors.toList());
+
+        for (Person p: people) {
+            System.out.println(p);
+        }
     }
 }
